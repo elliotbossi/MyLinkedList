b672ac065673e6eef804dc124c6a0df6309ae38e
Elliot <ebossi10@stuy.edu>
2021-01-06 14:00:03 -0500

    addded get/set for data

--- Node.java
+++ Node.java
@@ -1,1 +1,17 @@
+public class Node{
+ public Node(String v){/*create a constructor*/
+   data = v;
+ }
+ private String data;
+ private Node next,prev;
+ //write get/set methods for all three instance variables.
 
+ public String get(){
+   return  data;
+ }
+ public void set(String v){
+   data = v;
+ }
+
+
+}
