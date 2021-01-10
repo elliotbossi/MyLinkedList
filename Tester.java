261da7f2f55fb27480e2fecc30009f04c7171aa8
Elliot <ebossi10@stuy.edu>
2021-01-10 01:19:41 -0500

    upddated tesster

--- Tester.java
+++ Tester.java
@@ -21,7 +21,33 @@ public class Tester {
             System.out.println(linkedlist);
             linkedlist.set(1,"why");
             System.out.println(linkedlist.toString());
+            System.out.println(linkedlist.toStringReversed());
             /////
+
+            MyLinkedList linkedlist1 = new MyLinkedList();
+            MyLinkedList linkedlist2 = new MyLinkedList();
+
+            linkedlist1.add("0");
+            linkedlist1.add("1");
+            linkedlist1.add("2");
+            linkedlist1.add("3");
+            //
+            linkedlist2.add("4");
+            linkedlist2.add("5");
+            linkedlist2.add("6");
+            linkedlist2.add("7");
+            linkedlist1.extend(linkedlist2);
+
+            System.out.println(linkedlist1.toString());
+            System.out.println(linkedlist2.toString());
+
+            linkedlist1.remove(0);
+            linkedlist1.remove(6);
+            linkedlist1.remove(2);
+
+            System.out.println(linkedlist1.toString());
+
+
 
 
 }
